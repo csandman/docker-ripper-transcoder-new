8037a16593a2979e43a2533fb48348f2d91ccd6a
csandman <csandvik@uvm.edu>
2020-03-24 17:29:54 -0400

    Copy default MakeMKV profile

--- root/etc/my_init.d/ripper.sh
+++ root/etc/my_init.d/ripper.sh
@@ -2,26 +2,31 @@
 
 # copy default script
 if [[ ! -f /config/ripper.sh ]]; then
- cp /ripper/ripper.sh /config/ripper.sh
+  cp /ripper/ripper.sh /config/ripper.sh
 fi
 
 # copy default settings
-if [[ ! -f /config/settings.conf ]] && [[  ! -f /config/enter-your-key-then-rename-to.settings.conf ]]; then
- cp -f /ripper/settings.conf /config/
- mv /config/settings.conf /config/enter-your-key-then-rename-to.settings.conf 
+if [[ ! -f /config/settings.conf ]] && [[ ! -f /config/enter-your-key-then-rename-to.settings.conf ]]; then
+  cp -f /ripper/settings.conf /config/
+  mv /config/settings.conf /config/enter-your-key-then-rename-to.settings.conf
+fi
+
+# copy default profile
+if [[ ! -f /config/profile.mmcp.xml ]]; then
+  cp /ripper/profile.mmcp.xml /config/profile.mmcp.xml
 fi
 
 # fetching MakeMKV beta key
-KEY=$(curl --silent 'https://www.makemkv.com/forum/viewtopic.php?f=5&t=1053'| grep -oP 'T-[\w\d@]{66}')
+KEY=$(curl --silent 'https://www.makemkv.com/forum/viewtopic.php?f=5&t=1053' | grep -oP 'T-[\w\d@]{66}')
 
 # move settings.conf, if found
 mkdir -p /root/.MakeMKV
-if [[ -f  /config/settings.conf ]]; then
- echo "Found settings.conf. Replacing beta key file."
- cp -f  /config/settings.conf /root/.MakeMKV/
+if [[ -f /config/settings.conf ]]; then
+  echo "Found settings.conf. Replacing beta key file."
+  cp -f /config/settings.conf /root/.MakeMKV/
 elif ! [ "$KEY" = '' ]; then
- echo "Using MakeMKV beta key: $KEY"
- echo app_Key = "\"$KEY"\" > /root/.MakeMKV/settings.conf
+  echo "Using MakeMKV beta key: $KEY"
+  echo app_Key = "\"$KEY"\" >/root/.MakeMKV/settings.conf
 fi
 
 # permissions
